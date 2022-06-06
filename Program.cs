cfc4967f28c0a869609d54d75ed7b873c5e41c34
Jason Jia <jason.jia@laserfiche.com>
2022-06-06 09:24:22 -0400

    Read access key in base64 and decode

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Laserfiche.Api.Client.OAuth;
 using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Text;
 
 namespace Laserfiche.Repository.Api.Client.Sample.ServiceApp
 {
@@ -18,6 +19,15 @@ namespace Laserfiche.Repository.Api.Client.Sample.ServiceApp
 
             // Read credentials from envrionment
             var servicePrincipalKey = Environment.GetEnvironmentVariable("DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY");
+            var base64EncodedAccessKey = Environment.GetEnvironmentVariable("DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY");
+            if (base64EncodedAccessKey != null)
+            {
+                var decoded = Encoding.UTF8.GetString(Convert.FromBase64String(base64EncodedAccessKey));
+                Environment.SetEnvironmentVariable("DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY", decoded);
+            } else
+            {
+                throw new InvalidOperationException("Cannot continue due to missing access key.");
+            }
             var accessKey = JsonConvert.DeserializeObject<AccessKey>(Environment.GetEnvironmentVariable("DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY"));
             var repositoryId = Environment.GetEnvironmentVariable("DEV_CA_PUBLIC_USE_REPOSITORY_ID_1");
 
